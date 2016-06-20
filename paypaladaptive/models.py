5631e4f1fe671ec5a6939b1df19872fd4ca98eef
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2016-06-20 05:24:06 +0400

    Fix ShortUUIDField usage
    Due to uncommon API, the first argument to ShortUUIDField is `auto`, not `verbose_name`.
    
    Passing a `ugettext_lazy` result here gets it evaluated, and requires to load all applications,
    making it impossible to reliably refer to paypaladaptive.models in signal listeners or anything
    that's being used before all apps are properly initialized (as there will be a risk of circular imports).

--- paypaladaptive/models.py
+++ paypaladaptive/models.py
@@ -30,7 +30,7 @@ class PaypalAdaptive(models.Model):
     money = MoneyField(_(u'money'), max_digits=settings.MAX_DIGITS,
                        decimal_places=settings.DECIMAL_PLACES)
     created_date = models.DateTimeField(_(u'created on'), auto_now_add=True)
-    secret_uuid = ShortUUIDField(_(u'secret UUID'))  # to verify return_url
+    secret_uuid = ShortUUIDField(verbose_name=_(u'secret UUID'))  # to verify return_url
     debug_request = models.TextField(_(u'raw request'), blank=True, null=True)
     debug_response = models.TextField(_(u'raw response'), blank=True,
                                       null=True)
